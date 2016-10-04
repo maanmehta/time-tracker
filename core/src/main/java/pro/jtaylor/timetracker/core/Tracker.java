357df2b843d7cc2493b41c0f58928ab2e9798a52
Maan Mehta <msmgrd-adm@yahoo.com>
2016-10-03 22:21:39 -0400

    adding some code violations

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -16,6 +16,7 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
+        if (true)
         entries.remove(entry);
     }
 
@@ -24,6 +25,14 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
+        try {
+        } catch (exception e){
+        }
+        
+        boolean valid = false;
+        if (valid = false) {
+            //whatever
+        }
         return entries.get(index);
     }
 }
